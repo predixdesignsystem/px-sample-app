431276d580e8a66c2798fb1f8e3bf09bac25f245
David Leonard <davidrobertleonard@gmail.com>
2018-04-30 16:05:03 -0700

    Move to Apache-2.0 license

--- server/proxy.js
+++ server/proxy.js
@@ -1,4 +1,21 @@
 /**
+ * @license
+ * Copyright (c) 2018, General Electric
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * This module can be used to set up reverse proxying from client to Predix services.
  * It assumes only one UAA instance, one UAA client, and one instance of each service.
  * Use setUaaConfig() and setServiceConfig() for local development.
